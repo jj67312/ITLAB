872c5ade747d1fe6d8ae4c1c2fc13332ad8c55e6
Flux <discordaryan@gmail.com>
2023-06-24 00:33:26 +0530

    auth.ejs and auth.css added

--- authMiddleware.js
+++ authMiddleware.js
@@ -2,7 +2,7 @@ module.exports.isAuth = (req, res, next) => {
     if (req.isAuthenticated()) {
         next();
     } else {
-        res.send('You are not authorized to view this resource');
+        res.render('auth.ejs');
     }
 }
 
