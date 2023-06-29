60f88f4721927b9739b381407a7c9e0a2807f25c
Flux <discordaryan@gmail.com>
2023-06-29 17:19:54 +0530

    Market categories js configured

--- public/javascripts/market.js
+++ public/javascripts/market.js
@@ -13,3 +13,26 @@ dropdownBtn.addEventListener("click", () => {
     dropdownBtn.getAttribute("aria-expanded") === "true" ? "false" : "true"
   );
 });
+
+function hamToogle() {
+  document.getElementById("menu").classList.toggle("icon");
+  document.querySelector(".hamMenu").classList.toggle('hamChange');
+}
+
+const productGrids = document.getElementsByClassName("product-grid");
+productGrids[0].style.display="grid";
+  for(let i = 1; i<productGrids.length; i++) {
+    productGrids[i].style.display="none";
+  }
+
+function showProductGrid(categoryIndex) {
+  const productGrids = document.getElementsByClassName("product-grid");
+
+  for (let i = 0; i < productGrids.length; i++) {
+    if (i === categoryIndex) {
+      productGrids[i].style.display = "grid";
+    } else {
+      productGrids[i].style.display = "none";
+    }
+  }
+}
